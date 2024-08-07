be9f1a140ba616482ce379f85178b4776f36e3db
Vincent <vincentcan909@gmail.com>
2024-08-07 11:13:16 +0700

    Feat: add images feature

--- server/controllers/travelController.js
+++ server/controllers/travelController.js
@@ -1,4 +1,12 @@
-const { Travel, User, UserTravel } = require('../models');
+const { Travel, User, UserTravel, Image } = require('../models');
+const { v2: cloudinary } = require('cloudinary');
+
+cloudinary.config({
+    cloud_name: "diymv0tqb",
+    api_key: "428231918617181",
+    api_secret: "o8efN9jnCZcxy3HI2bk3X-ABLN8" 
+})
+
 
 class travelController{ 
     static async getTravel(req, res, next){
@@ -35,6 +43,43 @@ class travelController{
             console.log(error)
         }
     } 
+
+    static async addImageTravel(req, res, next){
+        const { id } = req.params;
+        try {
+            const travel = await Travel.findByPk(id);
+
+            if(!travel) throw { name: 'NotFound' };
+
+            const file = req.file;
+            const base64 = file.buffer.toString("base64");
+
+            const result = await cloudinary.uploader.upload(
+                `data:${file.mimetype};base64,${base64}`
+            );
+
+            const upload = await Image.create({ TravelId: id, imageUrl: result.secure_url })
+
+            res.status(200).json({imageUrl: result.secure_url});
+        } catch (error) {
+            next(error)
+            console.log(error)
+        }
+    } 
+
+    static async getImage(req, res, next){
+        const { id } = req.params;
+        try {
+            const data = await Image.findOne({ where: { TravelId: id } });
+
+            if(!data) throw { name: 'NotFound' }
+            
+            res.status(200).json(data)
+        } catch (error) {
+            next(error)
+            console.log(error)
+        }
+    }
 
     static async editTravel(req, res, next){
         const { id } = req.params;
--- server/routers/travelRouter.js
+++ server/routers/travelRouter.js
@@ -1,4 +1,5 @@
 const travelController = require('../controllers/travelController');
+const upload = require('../helpers/multer');
 
 const router = require('express').Router();
 
@@ -6,6 +7,8 @@ router.get('/', travelController.getTravel);
 router.post('/', travelController.addTravel);
 router.get('/userTravel', travelController.getUserTravel);
 router.post('/userTravel/:id/:UserId', travelController.joinTravel);
+router.get('/image/:id', travelController.getImage);
+router.post('/image/:id',upload.single('image'), travelController.addImageTravel);
 router.get('/:id', travelController.getTravelById);
 router.put('/:id', travelController.editTravel);
 
