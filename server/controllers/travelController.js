7899ca646c40b58fb2c2c53c94b817cde8572b59
Vincent <vincentcan909@gmail.com>
2024-08-09 03:04:36 +0700

    Feat: midtrans

--- server/controllers/travelController.js
+++ server/controllers/travelController.js
@@ -2,35 +2,36 @@ const { Op, where } = require('sequelize');
 const { Travel, User, UserTravel, Image, Category, sequelize, UserProfile } = require('../models');
 const { v2: cloudinary } = require('cloudinary');
 const { now } = require('sequelize/lib/utils');
+const midtransClient = require('midtrans-client');
 
 cloudinary.config({
     cloud_name: "diymv0tqb",
     api_key: "428231918617181",
-    api_secret: "o8efN9jnCZcxy3HI2bk3X-ABLN8" 
+    api_secret: "o8efN9jnCZcxy3HI2bk3X-ABLN8"
 })
 
 
-class travelController{ 
-    static async getTravel(req, res, next){
+class travelController {
+    static async getTravel(req, res, next) {
         const { categoryId, page, search } = req.query;
         try {
             const options = {
                 include: [{
                     model: Image
-                },{
+                }, {
                     model: Category,
                     where: categoryId ? { id: categoryId } : {}
-                },{
+                }, {
                     model: User
                 }],
-                where: search ? { destination: { [Op.iLike]: `%${search}%` }, deletedAt: null } : { deletedAt: null}
+                where: search ? { destination: { [Op.iLike]: `%${search}%` }, deletedAt: null } : { deletedAt: null }
             }
 
-            if(page){
-                if(page.limit){
+            if (page) {
+                if (page.limit) {
                     options.limit = page.limit
                 }
-                if(page.offset){
+                if (page.offset) {
                     options.offset = page.offset
                 }
             }
@@ -44,13 +45,13 @@ class travelController{
         }
     }
 
-    static async getTravelById(req, res, next){
+    static async getTravelById(req, res, next) {
         const { id } = req.params;
         try {
             const data = await Travel.findByPk(id, {
                 include: [{
                     model: Image
-                },{
+                }, {
                     model: User,
                     include: [
                         {
@@ -67,13 +68,13 @@ class travelController{
         }
     }
 
-    static async getRandomTravel(req, res, next){
+    static async getRandomTravel(req, res, next) {
         const { categoryId, page, search } = req.query;
         try {
             const options = {
                 include: [{
                     model: Image
-                },{
+                }, {
                     model: Category,
                     where: categoryId ? { id: categoryId } : {}
                 }],
@@ -81,11 +82,11 @@ class travelController{
                 order: sequelize.random()
             }
 
-            if(page){
-                if(page.limit){
+            if (page) {
+                if (page.limit) {
                     options.limit = page.limit
                 }
-                if(page.offset){
+                if (page.offset) {
                     options.offset = page.offset
                 }
             }
@@ -99,7 +100,7 @@ class travelController{
         }
     }
 
-    static async addTravel(req, res, next){
+    static async addTravel(req, res, next) {
         const { destination, description, price, capacity, CategoryId } = req.body;
         try {
             const data = await Travel.create({ destination, description, price, capacity, CategoryId });
@@ -109,15 +110,15 @@ class travelController{
             next(error)
             console.log(error)
         }
-    } 
+    }
 
-    static async deleteTravel(req, res, next){
+    static async deleteTravel(req, res, next) {
         const { id } = req.params;
         try {
             console.log('aaa')
             const data = await Travel.findByPk(id);
 
-            if(!data) throw { name: 'NotFound' };
+            if (!data) throw { name: 'NotFound' };
             // console.log(new Date())
             data.update({ deletedAt: new Date() });
 
@@ -128,17 +129,17 @@ class travelController{
             next(error)
             console.log(error)
             console.log('aaa')
-            
-        }
-    } 
-    
 
-    static async addImageTravel(req, res, next){
+        }
+    }
+
+
+    static async addImageTravel(req, res, next) {
         const { id } = req.params;
         try {
             const travel = await Travel.findByPk(id);
 
-            if(!travel) throw { name: 'NotFound' };
+            if (!travel) throw { name: 'NotFound' };
 
             const file = req.file;
             const base64 = file.buffer.toString("base64");
@@ -149,19 +150,19 @@ class travelController{
 
             const upload = await Image.create({ TravelId: id, imageUrl: result.secure_url })
 
-            res.status(200).json({imageUrl: result.secure_url});
+            res.status(200).json({ imageUrl: result.secure_url });
         } catch (error) {
             next(error)
             console.log(error)
         }
-    } 
+    }
 
-    static async getImage(req, res, next){
+    static async getImage(req, res, next) {
         const { id } = req.params;
         try {
             const data = await Image.findOne({ where: { TravelId: id } });
 
-            if(!data) throw { name: 'NotFound' }
+            if (!data) throw { name: 'NotFound' }
 
             res.status(200).json(data)
         } catch (error) {
@@ -170,13 +171,13 @@ class travelController{
         }
     }
 
-    static async editTravel(req, res, next){
+    static async editTravel(req, res, next) {
         const { id } = req.params;
         const { destination, description, price, capacity } = req.body;
         try {
             const data = await Travel.findByPk(id);
 
-            if(!data) throw { name: 'NotFound' }
+            if (!data) throw { name: 'NotFound' }
 
             data.update({ destination, description, price, capacity })
 
@@ -187,7 +188,7 @@ class travelController{
         }
     }
 
-    static async joinTravel(req, res, next){
+    static async joinTravel(req, res, next) {
         const { id } = req.params;
         try {
             const travel = await Travel.findByPk(id, {
@@ -196,17 +197,17 @@ class travelController{
                 }]
             });
 
-            if(!travel) throw { name: 'NotFound' }
+            if (!travel) throw { name: 'NotFound' }
 
-            if(travel.Users.length === travel.capacity) throw { name: 'TravelFull' };
+            if (travel.Users.length === travel.capacity) throw { name: 'TravelFull' };
 
             const userId = req.user.id;
- 
+
             const user = await User.findByPk(userId);
 
-            if(!user) throw { name: 'NotFound' }
+            if (!user) throw { name: 'NotFound' }
 
-            const data = await UserTravel.create({ UserId: user.id, TravelId: id});
+            const data = await UserTravel.create({ UserId: user.id, TravelId: id });
 
             res.status(201).json(data)
         } catch (error) {
@@ -215,12 +216,12 @@ class travelController{
         }
     }
 
-    static async deleteUserTravel(req, res, next){
+    static async deleteUserTravel(req, res, next) {
         const { id } = req.params;
         try {
             const userTravel = await UserTravel.findByPk(id);
 
-            if(!userTravel) throw { name: 'NotFound' }
+            if (!userTravel) throw { name: 'NotFound' }
 
             userTravel.destroy();
 
@@ -233,7 +234,7 @@ class travelController{
         }
     }
 
-    static async getUserTravel(req, res, next){
+    static async getUserTravel(req, res, next) {
         try {
             const data = await UserTravel.findAll();
 
@@ -244,11 +245,46 @@ class travelController{
         }
     }
 
-    static async getCategories(req, res, next){
+    static async getCategories(req, res, next) {
         try {
             const data = await Category.findAll();
 
             res.status(200).json(data)
+        } catch (error) {
+            next(error)
+            console.log(error)
+        }
+    }
+
+    static async generateMidtransToken(req, res, next) {
+        const { id } = req.params
+        try {
+            const travel = await Travel.findByPk(id);
+            if (!travel) throw { name: 'NotFound' }
+            const user = await User.findByPk(req.user.id)
+            if (!user) throw { name: 'NotFound' }
+
+            let snap = new midtransClient.Snap({
+                // Set to true if you want Production Environment (accept real transaction).
+                isProduction: false,
+                serverKey: 'SB-Mid-server-_twRyfjOKtn1sy_TU0m1SJJh'
+            });
+
+            let parameter = {
+                "transaction_details": {
+                    "order_id": "TRANSACTION_" + Math.floor(10000 + Math.random() * 90000),
+                    "gross_amount": travel.price
+                },
+                "credit_card": {
+                    "secure": true
+                },
+                "customer_details": {
+                    "email": user.email
+                }
+            };
+
+            const midtransToken = await snap.createTransaction(parameter)
+            res.status(201).json(midtransToken)
         } catch (error) {
             next(error)
             console.log(error)
--- server/routers/travelRouter.js
+++ server/routers/travelRouter.js
@@ -14,8 +14,11 @@ router.post('/userTravel/:id',authentication , travelController.joinTravel);
 router.delete('/userTravel/:id',authentication , travelController.deleteUserTravel);
 router.get('/image/:id',authentication, authorization, travelController.getImage);
 router.post('/image/:id',authentication ,upload.single('image'), travelController.addImageTravel);
+router.post('/generate-midtrans-token/:id',authentication , travelController.generateMidtransToken);
 router.get('/:id', travelController.getTravelById);
 router.put('/:id',authentication, authorization, travelController.editTravel);
 router.delete('/:id',authentication, authorization, travelController.deleteTravel);
 
 module.exports = router;
+
+// 	SB-Mid-server-_twRyfjOKtn1sy_TU0m1SJJh
